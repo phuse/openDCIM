ae54c245d33be77db9f0d8635e2cac6d1f4c955b
Daniel Hrynczenko <daniel@phu.se>
2014-02-17 17:02:37 +0100

    Update devices.php
    fixed a missed </div> for when seriallock is enabled

--- devices.php
+++ devices.php
@@ -1742,7 +1742,7 @@ echo '<div class="center"><div>
                    <div><label for="serialno">'.__("Serial Number").'</label></div>';
                    # lock the serial field if the device has already been added to ensure dataconsistency
                                 if ($dev->SerialNo && $config->ParameterArray["SerialLock"]=='Enabled'){
-echo '             <div><input type="text" name="serialno" id="serialno" size="40" value="'.$dev->SerialNo.'" readonly>';
+echo '             <div><input type="text" name="serialno" id="serialno" size="40" value="'.$dev->SerialNo.'" readonly></div>';
                    }else{
 echo '             <div><input type="text" name="serialno" id="serialno" size="40" value="'.$dev->SerialNo.'">
                    <button class="hide" type="button" onclick="getScan(\'serialno\')">',__("Scan Barcode"),'</button></div>';

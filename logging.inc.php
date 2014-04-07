e960b35c04a19f1b9ef73497d02633c849f3c609
Daniel Hrynczenko <daniel@phu.se>
2014-04-07 14:53:18 +0200

    Added funtion to display log per device

--- logging.inc.php
+++ logging.inc.php
@@ -92,6 +92,20 @@ class LogActions {
 
 
 	// Add in functions here for actions lookup by device, user, date, etc
+	static function ShowDeviceLog($device,$count){
+		global $dbh;
+		$sql="SELECT * FROM fac_DeviceLog WHERE DeviceID=\"$device\" ORDER BY Time ASC LIMIT 5;";
+		$dbh->query($sql);
+		$logList=array();
+                foreach($dbh->query($sql) as $row){
+                        $n=$row["LogID"]; // index array by id
+                        $logList[$n]->ColorID=$row["LogID"];
+                        $logList[$n]->Username=$row["UserID"];
+                        $logList[$n]->Action=$row["Action"];
+                        $logList[$n]->Time=$row["Time"];
+                }
+                return $logList;
+	}
 }
 
 ?>
